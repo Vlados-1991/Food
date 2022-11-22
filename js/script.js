3540b26859c75d3b053d5b7f197268284a925704
Vlad <vladlotarev@gmail.com>
2022-11-22 15:23:18 +0300

    05-16

--- js/script.js
+++ js/script.js
@@ -424,43 +424,24 @@
         });
     });
 
-//     showSlides(slideIndex);
+    // Calc
 
-    // if (slides.length < 10) {
-    //     total.textContent = `0${slides.length}`;
-    // } else {
-    //     total.textContent = slides.length;
-    // }
+    const result = document.querySelector('.calculating__result span');
+    let sex, height, weight, age, ratio;
 
-//     function showSlides(n) {
-//         if ( n > slides.length) {
-//             slideIndex = 1;
-//         }
+    function calcTotal() {
+        if (!sex || !height || !weight || !age || !ratio) {
+            result.textContent = '____';
+            return;
+        }
 
-//         if (n < 1) {
-//             slideIndex = slides.length;
-//         }
+        if (sex === 'female') {
+            result.textContent = (447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio;
+        } else {
+            result.textContent = (88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio;
+        }
+    }
 
-//         slides.forEach(item => item.style.display = 'none');
+    calcTotal();
 
-//         slides[slideIndex - 1].style.display = 'block';
-
-//         if (slides.length < 10) {
-//             current.textContent = `0${slideIndex}`;
-//         } else {
-//             current.textContent = slideIndex;
-//         }
-//     }
-
-//     function plusSlides(n) {
-//         showSlides(slideIndex += n);
-//     }
-
-//     prev.addEventListener('click', () => {
-//         plusSlides(-1);
-//     });
-
-//     next.addEventListener('click', () => {
-//         plusSlides(1);
-//     });
 });
